b23d80535a8c32e3503a43d72372af012e2ad764
Iqrakhan1 <ik22082002khan@gmail.com>
2024-08-21 23:47:57 +0530

    Functions task

---
+++ week_2/tasks/complex_primitives/01_arrays/reverse.js
@@ -0,0 +1,7 @@
+// reverse all the elements
+
+let arr = [2, 4, 5, 6, 7, 8, 9];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
---
+++ week_2/tasks/functions/index.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(45, 67));
+console.log(sum(135, 7879));
+console.log(sum(789, 987));
